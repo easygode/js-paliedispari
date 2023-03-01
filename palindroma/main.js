ac202dcfd691fe8d962008ff61d93be3a319e910
easygode <bimalbenvenuti@gmail.com>
2023-03-01 17:46:25 +0100

    esercizio palindroma nuovo readme e tentativo

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,24 +1,41 @@
 'use strict';
 
 //1. Creo una variabile con prompt per inserire una parola in formato stringa
-const input = prompt('Inserisci una parola');
-console.log(input);
+const inPut = prompt('Inserisci una parola');
+console.log(inPut);
 
 
-//2. Creo una funzione paliInFrasca per verificare se la parola è palindroma
-function paliInFrasca () {
-    let i = 0
- for (let i = 0; i < input.length; i++) {
-    console.log([i]);
+//2. Creo una funzione ailatIditaLia per verificare se la parola è palindroma
+function aiLatidItalia (inPut) {
+ for (let i = 0; i < inPut.length; i++) {
+    console.log({inPut});
+    return inPut;
  }
 }
 
-paliInFrasca();
+//3. Creo una funzione per invertire la parola
 
+function ailatIditaLia (inPut) {
+ for( let i = inPut.length-1 ;i >= 0 ;i--) {
+    console.log(putIn);
+    return putIn;
+ }
+}
 
-//3. Creo un ciclo for per ottenere un array delle lettere della parola da destra a sinistra
-//4. Creo un ciclo for inverso per ottenere un array delle lettere della parola da sinistra  a  destra
-//5. Creo una funzione che mi permette di comparare i due cicli
+aiLatidItalia ('anna');
+ailatIditaLia ('anna');
+
+//4. Creo una funzione per verificare se la parola è palindrome
    //SE gli array ottenuti sono uguali allora la parola è palindroma
-   //SE ALTRIMENTI gli array sono diversi allora la parola non è palindroma
-//6. Stampo a console PALINDROMA se la parola è palindroma, NON PALINDROMA se la parola non è palindroma
+      //SE ALTRIMENTI gli array sono diversi allora la parola non è palindroma
+function palindroma( inPut ) {
+   if( putIn === inPut){
+
+      //5. Stampo a console 'sono palindroma' se la parola è palindroma, 'non sono palindroma' se la parola non è palindroma
+        console.log('sono palindroma');
+   } else {
+        console.log('non sono palindroma')
+      }
+    }
+
+palindroma ('anna');
--- palindroma/readme.md
+++ palindroma/readme.md
@@ -3,10 +3,9 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
 1. Creo una variabile con prompt per inserire una parola in formato stringa
-2. Creo una funzione PaliInFrasca per verificare se la parola è palindroma
-3. Creo un ciclo for per ottenere un array delle lettere della parola da destra a sinistra
-4. Creo un ciclo for inverso per ottenere un array delle lettere della parola da sinistra  a  destra
-5. Creo una funzione che mi permette di comparare i due cicli
-   SE gli array ottenuti sono uguali allora la parola è palindroma
-   SE ALTRIMENTI gli array sono diversi allora la parola non è palindroma
-6. Stampo a console PALINDROMA se la parola è palindroma, NON PALINDROMA se la parola non è palindroma
+2. Creo una funzione per verificare se la parola è palindroma
+3. Creo una funzione per invertire la parola
+4. Creo una funzione per verificare se la parola è palindrome
+   //SE gli array ottenuti sono uguali allora la parola è palindroma
+      //SE ALTRIMENTI gli array sono diversi allora la parola non è palindroma
+5. Stampo a console 'sono palindroma' se la parola è palindroma, 'non sono palindroma' se la parola non è palindroma
